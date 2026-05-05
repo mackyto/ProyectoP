58fa005802d15f404167d9b46748fe366e628b82
Javier Simarro Olivares <javsimoli@alu.edu.gva.es>
2026-05-05 23:48:16 +0200

    UD3 new

--- src/main/java/persistencia/pClient.java
+++ src/main/java/persistencia/pClient.java
@@ -3,8 +3,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package persistencia;
+
 import entidades.Cliente;
 import interfaces.InClient;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
 /**
@@ -13,9 +18,34 @@ import java.util.List;
  */
 public class pClient implements InClient {
 
+    /**
+     * Guarda los datos de un cliente en la base de datos
+     * @param c tipo Cliente, datos del objeto a persistir.
+     * @return true si completa la transacción.
+     */
     @Override
     public boolean persistirCliente(Cliente c) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+
+        try (Connection conn = DriverManager.getConnection("jdbc:mysql://mysql.gamvers.xyz:3306/ProyectoP", "javier", "hqxjt8Z%")) {
+
+            PreparedStatement cps = conn.prepareStatement("INSERT INTO clientes (persona_id, fidelidad, email) VALUES (?, ?, ?)");
+            PreparedStatement pps = conn.prepareStatement("INSERT INTO clientes (id, nombre, apellidos, telefono) VALUES (?, ?, ?, ?)");
+            
+            pps.setInt(1, c.getId());
+            pps.setString(2, c.getNombre());
+            pps.setString(3, c.getApellidos());
+            pps.setString(4, c.getTelefono());
+            pps.executeUpdate();
+            cps.setInt(1, c.getId());
+            cps.setInt(2, c.getNivelFidelidad());
+            cps.setString(3, c.getEmail());
+            cps.executeUpdate();
+        } catch (SQLException sqlException){
+            
+        }
+        
+        return true;
+        
     }
 
     @Override
@@ -24,10 +54,9 @@ public class pClient implements InClient {
     }
 
     @Override
-    public Cliente recuperarCliennteByID(int id) {
+    public Cliente recuperarCliennteByID(int id
+    ) {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
-    
-    
-    
+
 }
--- src/main/java/proyecto2526/Main.java
+++ src/main/java/proyecto2526/Main.java
@@ -8,6 +8,7 @@ import interfaces.LogicaNegocio;
 import logica.GestorComercio;
 import entidades.*;
 import java.util.List;
+import java.util.Scanner;
 
 /**
  *
@@ -15,57 +16,117 @@ import java.util.List;
  */
 public class Main {
 
+    public static Scanner kl = new Scanner(System.in);
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         GeneradorArticulos generador = GeneradorArticulos.getInstancia();
-        GeneradorClientes generarCli = GeneradorClientes.getInstancia();
+        //    GeneradorClientes generarCli = GeneradorClientes.getInstancia();
         LogicaNegocio comercio = GestorComercio.getInstance();
         GestorComercio gestor = (GestorComercio) comercio;
+
+        String opcion = "";
         
-        try{
-        
+        do{
+            
+        try {
+            
+            System.out.println("Menú");
+            System.out.println("1 - Crear cliente.");
+            System.out.println("2 -");
+            System.out.println("3 - ");
+            System.out.println("4 - ");
+            System.out.println("5 -");
+            System.out.println("6 - ");
+            System.out.println("7 - ");
+
+            opcion = kl.nextLine();
+           
+            switch (opcion) {
+                
+                case "1": crearCliente(); break;                
+                case "2": ; break;                
+                case "3": ; break;                
+                case "4": ; break;                
+                case "5": ; break;                
+                case "6": ; break;                
+                case "7": ; break;
+                
+                
+                
+                
+                
+            }
+            
+            
+            
+            
+            
+            
+            
 
             generador.generarArticulos(comercio, 100, 70);
             generador.mostrarEstadisticas();
-            
-            generarCli.crearClientes(comercio, 80);
-            
+
+            //    generarCli.crearClientes(comercio, 80);
             gestor.imprimirListaClientes(gestor.buscarClientes("José"));
-            
+
             //gestor.imprimirClientes();
             //gestor.imprimirArticulos();
-            
-            
-        }catch(ErrorDatos er){
-            
+        } catch (ErrorDatos er) {
+
             System.out.println(er.getMessage());
-            
+
         }
-    
+
+        }while (!opcion.equalsIgnoreCase("q"));
         
     }
+
     /**
-     * Imprime cantidad de articulos generados. 
+     * Imprime cantidad de articulos generados.
+     *
      * @param generador instancia uníca generadora de articulos.
      * @throws entidades.ErrorDatos
      */
-    public static void listarArticulos(GeneradorArticulos generador) throws ErrorDatos{
-            
-            // Obtener listas específicas
-            List<ProductoFisico> productos = generador.getProductosFisicos();
-            List<Servicio> servicios = generador.getServicios();
-        
-            System.out.println("\nProductos físicos generados: " + productos.size());
-            System.out.println("Servicios generados: " + servicios.size());        
-        
+    public static void listarArticulos(GeneradorArticulos generador) throws ErrorDatos {
+
+        // Obtener listas específicas
+        List<ProductoFisico> productos = generador.getProductosFisicos();
+        List<Servicio> servicios = generador.getServicios();
+
+        System.out.println("\nProductos físicos generados: " + productos.size());
+        System.out.println("Servicios generados: " + servicios.size());
+
     }
- 
-    
-    
-    
-    
-    
-    
+
+    public static Cliente crearCliente() {
+
+        Cliente cl = null;
+
+        try {
+
+            System.out.println("Nombre: ");
+            String nombre = kl.nextLine();
+            System.out.println("Apellidos: ");
+            String apellidos = kl.nextLine();
+            System.out.println("telefono: ");
+            String telefono = kl.nextLine();
+            System.out.println("Fidelidad: ");
+            int fidelidad = Integer.parseInt(kl.nextLine());
+            System.out.println("email: ");
+            String email = kl.nextLine();
+
+            cl = new Cliente(email, fidelidad, nombre, apellidos, telefono);
+
+        } catch (ErrorDatos ed) {
+            ed.printStackTrace();
+        }
+
+        return cl;
+
+    }
+
 }
